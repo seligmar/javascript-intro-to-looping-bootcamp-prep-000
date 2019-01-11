de0fa0b133188335a620471a41ab23274d539c10
Mary Selig <seligmar@gmail.com>
2019-01-11 14:28:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,7 +24,7 @@ function incrementVariable() {
 }
 do { 
   incrementVariable(); 
-  return array.shift(array);   
+  return array.shift();   
 } 
 while (array.length > 0 && incrementVariable());
 } 
