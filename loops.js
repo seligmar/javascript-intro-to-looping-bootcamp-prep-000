bbdc526f5e266abf4ddaac59767af1e6f6e7d69a
Mary Selig <seligmar@gmail.com>
2019-01-11 14:22:35 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+var array = []; 
+
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+  array.push(`I am ${i} strange loop.`)}  
+  else array.push(`I am ${i} strange loops.`);   
+} 
+  return array;  
+} 
+ 
+
+function whileLoop(n) {
+  let countdown = n; 
+  while (countdown > 0) {
+  console.log(--countdown)}
+  return `done`
+}
+
+function doWhileLoop(array) {
+  var i = array.length;
+function incrementVariable() {
+  i -= 1; 
+  return array; 
+}
+do { 
+  incrementVariable() 
+} 
+while (array.length > 0 && incrementVariable()); 
+} 
